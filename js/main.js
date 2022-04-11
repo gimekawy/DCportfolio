11a1b0d2b9c45c5f373dda36ee01b791c5fddb48
gimekawy <kamilw-67@wp.pl>
2022-04-11 22:08:16 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+
+
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('navigation--active');
+   
+}
+
+hamburger.addEventListener('click', handleClick);
+
+
+// animations
+
+gsap.registerPlugin(ScrollTrigger);
+var tl = gsap.timeline();
+
+tl.from('.content', {
+    y: '-30%',
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut
+})
+
+tl.from('.stagger1', {
+    opacity: 0,
+    y: -50,
+    stagger: .3,
+    ease: Power4.easeOut,
+    duration: 2
+}, "-=1.5")
+
+gsap.from(".transition2", {
+    scrollTrigger: {
+        trigger: '.transition2',
+        start: "top bottom"
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    stagger: .3
+})
+
+gsap.from(".transition3", {
+    scrollTrigger: {
+        trigger: ".transition3",
+        start: "top center"
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    stagger: .6
+})
+
+
+
+// carousel glider from opinions section
+
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+      
+    }
+  });
+
+//   window.addEventListener('load', function(){
+//     new Glider(document.querySelector('.glider'), {
+//         slidesToShow: 10
+//     })
+//   })
+
+
+// glide
+
+import Glide from '@glidejs/glide';
+
+new Glide('.glide').mount();
+
+// import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+
+// new Glide('.glide').mount({ Controls, Breakpoints });
+
+// glide end
